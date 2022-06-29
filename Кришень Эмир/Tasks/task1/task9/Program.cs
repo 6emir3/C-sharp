cfead5e2cc2c80bd8c099b0ec29be2c735260509
6emir3 <106762040+6emir3@users.noreply.github.com>
2022-06-29 23:51:42 +0300

    .

---
+++ Кришень Эмир/Tasks/task1/task9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number%10-number/10%10 == number / 10 % 10 - number / 100)
+            {
+                Console.WriteLine($"Цифры числа {number} образуют арифмитическую прогрессию");
+            }
+            else
+            {
+                Console.WriteLine($"Цифры числа {number} не образуют арифмитическую прогрессию");
+            }
+        }
+    }
+}
