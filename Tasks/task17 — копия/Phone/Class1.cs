0903522af937acbe7b35913ca0b90c6c9b232fac
6emir3 <106762040+6emir3@users.noreply.github.com>
2022-12-18 01:48:46 +0300

    h

---
+++ Tasks/task17 — копия/Phone/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Phone
+{
+    public class Phone
+    {
+        string model;
+        string manufacturer;
+        double price;
+        bool available;
+
+        public Phone(string model,string manufacturer,double price,bool available)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+            this.available = available;
+        }
+
+        public void Discount(int discount)
+        {
+            price *= discount / 100.0;
+        }
+        public string Model
+        {
+            get { return model; }
+        }
+        public string Manufacturer
+        {
+            get { return manufacturer; }    
+        }
+        public double Price 
+        { 
+            get { return price; } 
+        }
+        public bool IsAvailable
+        {
+            get { return available; }
+        }
+        
+    }
+}
